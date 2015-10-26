be272a10bb3f54a56f3c7d6436ae221ee7f46cc1
synesthesio <msinger681@gmail.com>
2015-10-26 16:37:57 -0400

    basic project template w/coredata sans tests

---
+++ relephant/relephant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  relephant
+//
+//  Created by synesthesia on 10/26/15.
+//  Copyright © 2015 synesthesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
