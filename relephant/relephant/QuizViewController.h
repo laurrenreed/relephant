b651bb5590e0c13708bc76095d44c48aec944f24
laurrenreed <lauren.reed@me.com>
2015-10-29 14:15:33 -0400

    changed to storyboard - added emoji quiz page

---
+++ relephant/relephant/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  relephant
+//
+//  Created by Lauren Reed on 10/28/15.
+//  Copyright © 2015 synesthesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
