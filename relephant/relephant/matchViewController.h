b01282f8ba459a11e956f6045449c65807a0b4a6
synesthesio <msinger681@gmail.com>
2015-11-01 12:00:59 -0500

    added userclass props

---
+++ relephant/relephant/matchViewController.h
@@ -0,0 +1,13 @@
+//
+//  matchViewController.h
+//  relephant
+//
+//  Created by synesthesia on 11/1/15.
+//  Copyright © 2015 synesthesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface matchViewController : UIViewController
+
+@end
